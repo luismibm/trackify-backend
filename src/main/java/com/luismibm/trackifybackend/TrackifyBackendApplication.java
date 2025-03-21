08b4901933b8351225966188b516a458b5e8277e
luismibm <luibenmor@gmail.com>
2025-03-21 13:00:06 +0100

    spring template

---
+++ src/main/java/com/luismibm/trackifybackend/TrackifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.luismibm.trackifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackifyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackifyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luismibm/trackifybackend/TrackifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luismibm.trackifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
