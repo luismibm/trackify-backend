c31de112a13d0b92caf8fe38509bcf8959025ae0
luismibm <luibenmor@gmail.com>
2025-05-22 14:43:43 +0200

    deploy test 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
@@ -52,11 +51,4 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
-    }
 }
